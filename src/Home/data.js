361120d222fa0dee063e9dc0e65cd63db0a25ff4
ruba <ruba.muhammed01@gmail.com>
2024-10-18 00:47:12 +0530

    'redux'

---
+++ src/Home/data.js
@@ -0,0 +1,20 @@
+export const data=[
+    {
+        "id":1,
+        "name":"iPhone 16 Pro Max",
+        "cat":"Phone",
+        "price":"1,49,900",
+    },
+    {
+        "id":2,
+        "name":"Samsung Galaxy S24 Ultra",
+        "cat":"Phones",
+        "price":"1,21,999",
+    },
+    {
+        "id":3,
+        "name":"Apple MacBook Air",
+        "cat":"Laptop",
+        "price":"1,34,900",
+    }
+]
